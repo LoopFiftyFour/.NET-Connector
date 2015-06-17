d1e8f2076fc7f093e112f33343f28a85bd5041a8
Joel W Kall <joel@loop54.com>
2015-06-17 11:56:47 +0200

    New model classes (since they were in Public.dll)

---
+++ Loop54/Model/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Loop54.Model
+{
+    public class Entity
+    {
+        public string EntityType { get; set; }
+        public string ExternalId { get; set; }
+
+        public Dictionary<string, List<object>> Attributes { get; private set; }
+
+        public Entity(string entityType, string externalId)
+        {
+            EntityType = entityType;
+            ExternalId = externalId;
+
+            Attributes = new Dictionary<string, List<object>>();
+        }
+
+
+    }
+
+}
---
+++ Loop54/Model/Event.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Loop54.Model
+{
+    public class Event
+    {
+        public string Type { get; set; }
+
+        public string String { get; set; }
+
+        public Entity Entity { get; set; }
+
+        public double Revenue = 0{ get; set; }
+
+        public string OrderId{ get; set; }
+
+        public int Quantity = 1{ get; set; }
+    }
+}
