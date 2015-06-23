2784e66492dfafc5ac343d6c7528715071683830
Joel W Kall <joel@loop54.com>
2015-06-23 13:19:53 +0200

    Documentet properties in Entity and Event

--- Loop54/Model/Entity.cs
+++ Loop54/Model/Entity.cs
@@ -5,9 +5,19 @@ namespace Loop54.Model
     public class Entity
     {
       
+        /// <summary>
+        /// Type of entity
+        /// </summary>
         public string EntityType { get; private set; }
+
+        /// <summary>
+        /// Id of entity
+        /// </summary>
         public string ExternalId { get; private set; }
 
+        /// <summary>
+        /// All attribute data stored on the entity. Each attribute has a key and a list of values.
+        /// </summary>
         public Dictionary<string, List<object>> Attributes { get; private set; }
 
         public Entity(string entityType, string externalId)
--- Loop54/Model/Event.cs
+++ Loop54/Model/Event.cs
@@ -3,16 +3,34 @@ namespace Loop54.Model
 {
     public class Event
     {
+        /// <summary>
+        /// Type of event, for instance "click", "addtocart" or "purchase". Not case-sensitive.
+        /// </summary>
         public string Type { get; set; }
 
+        /// <summary>
+        /// Use this property if the event was initiated on a string.
+        /// </summary>
         public string String { get; set; }
 
+        /// <summary>
+        /// Use this property if the event was initiated on an entity.
+        /// </summary>
         public Entity Entity { get; set; }
 
+        /// <summary>
+        /// The revenue from each item in the event. 
+        /// </summary>
         public double Revenue { get; set; }
 
+        /// <summary>
+        /// If the event was part of an order or group, set the order Id here.
+        /// </summary>
         public string OrderId{ get; set; }
 
+        /// <summary>
+        /// If the event involved multiple identical strings or entities, set the quantity here. Defaults to 1.
+        /// </summary>
         public int Quantity { get; set; }
 
         public Event()
