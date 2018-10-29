71b7d4c3d76dfab7512fa0968bf8ab4d1246ce66
Evgeny Morozov <evgeny@loop54.com>
2018-10-29 16:10:53 +0100

    Update ASP.NET Core version

--- Loop54.Shared/Properties/AssemblyInfo.cs
+++ Loop54.Shared/Properties/AssemblyInfo.cs
@@ -43,7 +43,7 @@ namespace Loop54.Properties
     {
         public const string Major = "5";
         public const string Minor = "0";
-        public const string Patch = "1";
+        public const string Patch = "2";
 
         public const string Full = Major + "." + Minor + "." + Patch;
     }
@@ -94,3 +94,4 @@ namespace Loop54.Properties
 // 4.2.0 Better support for custom request data 
 // 5.0.0 Added code samples, removed SearchResponse.Query and AutoCompleteResponse.Query and added support for ScopeAttributeName in scoped AutoComplete
 // 5.0.1 Run the scoped query autocomplete test in the code examples project. Changed some code example names and printouts. Added a license document.
+// 5.0.2 Update ASP.NET Core version
