a255230e6e5f3c50b2ed3040a93eb337d6ad536e
Evgeny Morozov <evgeny@loop54.com>
2022-06-27 15:20:30 +0200

    y7tKVkmi: Update NuGet package specification with correct dependency versions, add icon

--- Loop54.Shared/Properties/AssemblyInfo.cs
+++ Loop54.Shared/Properties/AssemblyInfo.cs
@@ -45,7 +45,7 @@ namespace Loop54.Properties
     {
         public const string Major = "5";
         public const string Minor = "6";
-        public const string Patch = "1";
+        public const string Patch = "2";
 
         public const string Full = Major + "." + Minor + "." + Patch;
     }
@@ -111,3 +111,4 @@ namespace Loop54.Properties
 // 5.5.0 Added "getBasketRecommendations".
 // 5.6.0 Upgraded .NET Framework from 4.5 (out of support) to 4.6.2 and Newtonsoft.Json from 10.0.3 to 13.0.1
 // 5.6.1 Upgraded System.Text.Encodings.Web to 4.5.1
+// 5.6.2 Updated NuGet package specification with correct dependency versions, added icon
